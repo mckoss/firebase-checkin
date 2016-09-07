064f486f3ed4a530ac1814bd1c7d6cb9f14dddb6
Mike Koss <koss@google.com>
2016-09-06 17:59:03 -0700

    Import firebase rest client and node and Promise types.

--- src/test/checkin-test.ts
+++ src/test/checkin-test.ts
@@ -1,7 +1,12 @@
 import {assert} from 'chai';
 import {Checkin} from '../checkin';
+import * as rest from '../firebase-rest';
+
+let secrets = require('../../auth-secrets');
 
 suite("Checkin tests.", () => {
+  let client = new rest.Client(secrets.APP, secrets.SECRET);
+
   test("Constructor", () => {
     let c = new Checkin();
     assert.isNotNull(c);
