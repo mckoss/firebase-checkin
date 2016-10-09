88d0625465c17dd8412cb01f2de80c2cb658d298
Mike Koss <koss@google.com>
2016-10-08 18:22:20 -0700

    configure multiple deployment

--- src/checkin.ts
+++ src/checkin.ts
@@ -1,9 +1,11 @@
 import * as firebase from 'firebase';
 
+let config = require('./config');
+
 export class Checkin {
   app: firebase.app.App;
 
   constructor() {
-    this.app = firebase.initializeApp({});
+    this.app = firebase.initializeApp(config);
   }
 }
--- src/test/checkin-test.ts
+++ src/test/checkin-test.ts
@@ -1,12 +1,7 @@
 import {assert} from 'chai';
 import {Checkin} from '../checkin';
-import * as rest from '../firebase-rest';
-
-let secrets = require('../../auth-secrets');
 
 suite("Checkin tests.", () => {
-  let client = new rest.Client(secrets.APP, secrets.SECRET);
-
   test("Constructor", () => {
     let c = new Checkin();
     assert.isNotNull(c);
